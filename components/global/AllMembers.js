b0bcbe96c4cac9a91c62128b92687eee5bfaed08
varduhimehrabyan <varduhimehrabyan@mail.ru>
2022-09-06 17:58:03 +0400

    scroll 1 slide

--- components/global/AllMembers.js
+++ components/global/AllMembers.js
@@ -4,7 +4,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Slider from "react-slick";
 
-
 import TeamMember from "./TeamMember";
 
 export default function AllMembers(props) {
@@ -14,11 +13,11 @@ export default function AllMembers(props) {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     initialSlide: 0,
-    autoplay:true,
-    autoplaySpeed:1800,
-  }
+    autoplay: true,
+    autoplaySpeed: 1800,
+  };
   useEffect(() => {
     AOS.init({
       // once: true,
@@ -26,15 +25,11 @@ export default function AllMembers(props) {
   }, []);
 
   return (
-    <div
-      data-aos="fade-up"
-      data-aos-duration="2000"
-    >
+    <div data-aos="fade-up" data-aos-duration="2000">
       <div className="relative z-10 px-4 md:px-[3rem]  mb-[0.531rem] md:mb-[4rem]">
-        
         <Slider {...settings}>
           {props.members.map((member) => (
-              <TeamMember member={member}  key={member.id}/>
+            <TeamMember member={member} key={member.id} />
           ))}
         </Slider>
       </div>
